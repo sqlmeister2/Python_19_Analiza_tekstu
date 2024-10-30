a007b69c641786724567407d73bc2a8e2f6013fc
MarioPC <sqlmeister2@gmail.com>
2024-10-30 20:20:38 +0100

    Weryfikacja kodu i modyfikacja

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-plik = open("tekst.txt", "r")
+import os
+
+sciezka_skryptu = os.path.abspath(__file__)
+sciezka_bez_pliku = os.path.dirname(sciezka_skryptu)
+print(sciezka_bez_pliku)
+sciezka_z_plikiem_txt = sciezka_bez_pliku + '\\tekst.txt'
+print(sciezka_z_plikiem_txt)
+plik = open(sciezka_z_plikiem_txt, "r")
 tekst = plik.read()
+print(tekst)
 plik.close()
 
 #liczenie ilosci wystapienia litery w calym pliku
-
 def policz(txt, znak):
     licznik = 0
     for z in txt:
+        # print(z) #print pokazuje nam, że tekst pobrany z pliku jest stringiem i można po nim iterować pojedyncze litery
         if z == znak:
             licznik += 1
     return licznik
@@ -21,3 +29,5 @@ for z in "abcdefghijklmnoprstowxyz": # iteruje po każdej literze, bo to jest tr
     ile = policz(tekst.lower(), z)
     procent = ile * 100 / len(tekst)
     print("{0} - {1} - {2}%".format(z.upper(), ile, round(procent, 2)))
+
+
